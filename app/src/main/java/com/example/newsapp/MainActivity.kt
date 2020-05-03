2df4ec4fccc991faa48a59af352f71e9b7d914d3
Michael Peer <44434337+michael-peer@users.noreply.github.com>
2020-05-03 16:21:46 +0300

    Simple auth + bottom nav

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,14 +14,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_activity)
         setSupportActionBar(findViewById(R.id.my_toolbar))
         val navController = this.findNavController(R.id.nav_host_fragment)
-        NavigationUI.setupActionBarWithNavController(this,navController)
+        bottom_nav_view.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController)
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
-val  navController = this.findNavController(R.id.nav_host_fragment)
+        val navController = this.findNavController(R.id.nav_host_fragment)
         return navController.navigateUp()
     }
+
 
 }
 
